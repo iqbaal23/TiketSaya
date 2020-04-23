f2f25019a0e1e71f678503a8fdf121abcc10de62
iqbaal23 <11651100294@students.uin-suska.ac.id>
2020-04-24 06:39:46 +0700

    selesai bag 8

--- app/src/main/java/com/example/tiketsaya/TicketCheckoutAct.java
+++ app/src/main/java/com/example/tiketsaya/TicketCheckoutAct.java
@@ -6,20 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class TicketDetailAct extends AppCompatActivity {
+public class TicketCheckoutAct extends AppCompatActivity {
+
     Button btn_buy_ticket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ticket_detail);
+        setContentView(R.layout.activity_ticket_checkout);
 
         btn_buy_ticket = findViewById(R.id.btn_buy_ticket);
         btn_buy_ticket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent gotocheckout = new Intent(TicketDetailAct.this, TicketCheckoutAct.class);
-                startActivity(gotocheckout);
+                Intent gotosuccessticket = new Intent(TicketCheckoutAct.this, SuccessBuyTicketAct.class);
+                startActivity(gotosuccessticket);
             }
         });
     }
