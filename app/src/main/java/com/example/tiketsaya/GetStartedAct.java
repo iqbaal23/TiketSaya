7150b58c6b94f1c7c061580b236d0e356e94d1b7
iqbaal23 <11651100294@students.uin-suska.ac.id>
2020-04-25 22:49:01 +0700

    animation get started

--- app/src/main/java/com/example/tiketsaya/GetStartedAct.java
+++ app/src/main/java/com/example/tiketsaya/GetStartedAct.java
@@ -4,20 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GetStartedAct extends AppCompatActivity implements View.OnClickListener{
     Button btn_sign_in, btn_new_account_create;
+    Animation ttb, btt;
+    ImageView emblem_app;
+    TextView intro_app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
 
+        ttb = AnimationUtils.loadAnimation(this, R.anim.ttb);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
         btn_sign_in = findViewById(R.id.btn_sign_in);
         btn_sign_in.setOnClickListener(this);
         btn_new_account_create = findViewById(R.id.btn_new_account_create);
         btn_new_account_create.setOnClickListener(this);
+
+        emblem_app = findViewById(R.id.emblem_app);
+        intro_app = findViewById(R.id.intro_app);
+
+        emblem_app.startAnimation(ttb);
+        intro_app.startAnimation(ttb);
+        btn_sign_in.startAnimation(btt);
+        btn_new_account_create.startAnimation(btt);
     }
 
     @Override
